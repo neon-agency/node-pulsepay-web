c891f583d9491a1375b6488b493a46ed012a05dd
jvcorado <jhonisilva545@gmail.com>
2026-03-26 02:06:34 -0300

    refactor: Remove authentication middleware from resolve-credential route in bot.routes.js

--- src/routes/bot.routes.js
+++ src/routes/bot.routes.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const credentialsController = require('../controllers/credentials.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
 const asyncHandler = require('../utils/async-handler');
 
 const botRoutes = Router();
 
-botRoutes.post('/resolve-credential', authMiddleware, asyncHandler((req, res) => credentialsController.resolveForBot(req, res)));
+botRoutes.post('/resolve-credential', asyncHandler((req, res) => credentialsController.resolveForBot(req, res)));
 
 module.exports = botRoutes;
